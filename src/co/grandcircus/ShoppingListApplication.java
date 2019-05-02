8d01650365d435c615f37826aa6af91efa7bf03b
kuruvillabose <kuruvillamb@gmail.com>
2019-05-02 18:40:58 -0400

    Second commit

--- src/co/grandcircus/ShoppingListApplication.java
+++ src/co/grandcircus/ShoppingListApplication.java
@@ -12,6 +12,7 @@ public class ShoppingListApplication {
 		ArrayList<Double> price = new ArrayList<>();
 		ArrayList<String> itemOrdered = new ArrayList<>();
 		ArrayList<Double> priceOrd = new ArrayList<>();
+		
 		String prompt = "Y";
 		String pitems;
 
@@ -38,16 +39,17 @@ public class ShoppingListApplication {
 			System.out.println("==========================");
 			// for loop to display Array list
 			for (int i = 0; i < item.size(); i++) {
-				System.out.println(String.format("%-10s %s", item.get(i),"$", price.get(i)));
+				System.out.println(String.format("%-20s %s", item.get(i), price.get(i)));
 			}
 			boolean f = false;
 			int i = 0;
 			System.out.println("What Item would you like to order?");
 			pitems = scan.next();
+			
 			try {
 				for (i = 0; i < item.size(); i++) {
 					if (pitems.equals(item.get(i))) {
-						
+
 						itemOrdered.add(pitems);
 						priceOrd.add(price.get(i));
 						f = true;
@@ -71,21 +73,43 @@ public class ShoppingListApplication {
 		} while (prompt.equalsIgnoreCase("Y"));
 
 		System.out.println("Thanks for your order!");
-		
-			for (int i = 0; i < itemOrdered.size(); i++) {
-				System.out.println(String.format("%-20s %s", itemOrdered.get(i),"$", priceOrd.get(i)));
-			}
-			System.out.println("Average price per item in order was "+averageOfItems(priceOrd,itemOrdered.size()));
-		
+
+		for (int i = 0; i < itemOrdered.size(); i++) {
+			System.out.println(String.format("%-10s %s", itemOrdered.get(i), priceOrd.get(i)));
+		}
+		System.out.println("Average price per item in order was " + averageOfItems(priceOrd, itemOrdered.size()));
+		System.out.println("Item with highest cost is " + maxPrice(priceOrd, itemOrdered.size()));
+		System.out.println("Item with lowest cost is " + minPrice(priceOrd, itemOrdered.size()));
 		scan.close();
 	}
-	//Method1-Average of items ordered.
-	public static double averageOfItems(ArrayList<Double> items,int size){
-		double avg=0.0;
+
+	// Method1-Average of items ordered.
+	public static double averageOfItems(ArrayList<Double> items, int size) {
+		double avg = 0.0;
 		for (int i = 0; i < size; i++) {
-			avg+= items.get(i)/size;
+			avg += items.get(i) / size;
 		}
 		return avg;
-		
 	}
-	}	
+	//Method 2-Item with highest cost
+	public static double maxPrice(ArrayList<Double> price, int size) {
+		double max = price.get(0);
+		for (int i = 0; i < size; i++) {
+			
+			if(max<price.get(i))
+				max=price.get(i);
+		}
+		return max;
+	}
+	
+	//Method 3-Item with lowest cost
+		public static double minPrice(ArrayList<Double> price, int size) {
+			double min = price.get(0);
+			for (int i = 0; i < size; i++) {
+				
+				if(min>price.get(i))
+					min=price.get(i);
+			}
+			return min;
+		}
+}
